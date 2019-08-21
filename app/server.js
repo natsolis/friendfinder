dbe4304a66dd6f275acf8cae3c1e6a2ce4f1c579
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-08-21 14:29:53 -0600

    adding the routes

--- app/server.js
+++ app/server.js
@@ -1,11 +1,16 @@
 // requiring the npm packages express and the path
  var express = require("express");
+ var bodyParser = require("body-parser");
  var path = require("path");
 
 //  set up the express app 
 var app = express();
-var PORT = 3000;
+var port = process.env. PORT|| 8000 ;
 
 // set up the express app to handle data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes 
+require("./routing")
